ffc96a5dbc706f01b0695fd0a5463bad31f108a2
Kern Grant <kerndg@gmail.com>
2025-11-04 14:31:03 +0000

    Added LogsController, dedicated page/views for Logs and LogDetails, added navigation to the logs page on top bar

---
+++ UserManagement.Web/Controllers/Logging/LogsController.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using UserManagement.Services.Interfaces;
+
+namespace UserManagement.WebMS.Controllers
+{
+    [Route("logs")]
+    public class LogsController : Controller
+    {
+        private readonly ILogService _logService;
+
+        public LogsController(ILogService logService)
+        {
+            _logService = logService;
+        }
+        
+        [HttpGet("list")]
+        public IActionResult List()
+        {
+            var logs = _logService.GetLogs()
+                                  .OrderByDescending(l => l.Timestamp)
+                                  .ToList();
+
+            return View(logs);
+        }
+
+        [HttpGet("details/{id:int}")]
+        public IActionResult Details(int id)
+        {
+            var log = _logService.GetLogs().FirstOrDefault(l => l.Id == id);
+            if (log == null)
+                return NotFound();
+
+            return View(log);
+        }
+    }
+}
